2e9fdff74c53bff651bf35a0131560071a64e684
vitaly <vitalydev1337@gmail.com>
2020-07-31 13:46:15 +0100

    react 2nd try

---
+++ build/precache-manifest.d04a45f03eb62aed3187aea42b1a1294.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17ab23aa87624fe1ffe2735db55ac2d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c7b3cf8270833a6a009",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "daa57ecaa972de4a7b2b",
+    "url": "/static/js/2.c3dace66.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c3dace66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c7b3cf8270833a6a009",
+    "url": "/static/js/main.b95587e3.chunk.js"
+  },
+  {
+    "revision": "43f4a519bc2963c250aa",
+    "url": "/static/js/runtime-main.11d5dc12.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
