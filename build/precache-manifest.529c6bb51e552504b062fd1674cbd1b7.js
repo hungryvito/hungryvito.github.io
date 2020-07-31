5aaf49f3630abca74eb8fa84b7708a394284e254
vitaly <vitalydev1337@gmail.com>
2020-07-31 19:07:39 +0100

    minor changes

--- build/precache-manifest.529c6bb51e552504b062fd1674cbd1b7.js
+++ build/precache-manifest.529c6bb51e552504b062fd1674cbd1b7.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "17ab23aa87624fe1ffe2735db55ac2d0",
+    "revision": "ebe7b1327ce5f3720395918383dc610a",
     "url": "/index.html"
   },
   {
-    "revision": "9c7b3cf8270833a6a009",
-    "url": "/static/css/main.5f361e03.chunk.css"
+    "revision": "d97d55860303553ae957",
+    "url": "/static/css/main.523f09c5.chunk.css"
   },
   {
     "revision": "daa57ecaa972de4a7b2b",
@@ -16,15 +16,15 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.c3dace66.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "9c7b3cf8270833a6a009",
-    "url": "/static/js/main.b95587e3.chunk.js"
+    "revision": "d97d55860303553ae957",
+    "url": "/static/js/main.66eb0594.chunk.js"
   },
   {
     "revision": "43f4a519bc2963c250aa",
     "url": "/static/js/runtime-main.11d5dc12.js"
   },
   {
-    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
-    "url": "/static/media/logo.5d5d9eef.svg"
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
   }
 ]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d04a45f03eb62aed3187aea42b1a1294.js"
+  "/precache-manifest.529c6bb51e552504b062fd1674cbd1b7.js"
 );
 
 self.addEventListener('message', (event) => {
